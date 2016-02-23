de0cdfe4311419fed88c4faf42a6c8277e594f08
Taru Peltola <taru.peltola@cs.tamk.fi>
2016-02-23 14:28:22 +0200

    Sql database queries

--- sql/superchat.sql
+++ sql/superchat.sql
@@ -1,22 +1,22 @@
 CREATE TABLE Account
 (
-	client_id  	INTEGER	 	NOT NULL PRIMARY KEY UNIQUE
-	, username 	VARCHAR(16)	NOT NULL UNIQUE
-	, password 	VARCHAR(255) 	NOT NULL
+        client_id       INTEGER        NOT NULL PRIMARY KEY UNIQUE
+        , username      VARCHAR(16)    NOT NULL UNIQUE
+        , password      VARCHAR(255)   NOT NULL
 );
 
 CREATE TABLE Message
 (
-	message_id		INTEGER		NOT NULL PRIMARY KEY UNIQUE
-	, created_at 	TIMESTAMP 	NOT NULL CURRENT_TIMESTAMP
-	, username		VARCHAR(16)
+        message_id      INTEGER         NOT NULL PRIMARY KEY UNIQUE
+        , created_at    TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP
+        , username      VARCHAR(16)     NOT NULL
 );
 
 CREATE TABLE Message_Body
 (
-	message_id		INTEGER		NOT NULL PRIMARY KEY UNIQUE
-	, content		TEXT		
-);
+        message_id              INTEGER         NOT NULL PRIMARY KEY UNIQUE
+        , content               TEXT            NOT NULL
+); 
 
 CREATE TABLE Server_Connection
 (
@@ -32,3 +32,5 @@ CREATE TABLE Server_Keys
 (
 
 );
+
+SHOW TABLES;
