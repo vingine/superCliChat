a22171db29a3c54a71536933867cd41d2d82f28a
Taru Peltola <taru.peltola92@gmail.com>
2016-03-14 13:21:11 +0200

    Added file for creating database. Database and tables are now created.

--- sql/superchat.sql
+++ sql/superchat.sql
@@ -1,8 +1,8 @@
 CREATE TABLE Account
 (
-        client_id       INTEGER        NOT NULL PRIMARY KEY UNIQUE
-        , username      VARCHAR(16)    NOT NULL UNIQUE
-        , password      VARCHAR(255)   NOT NULL
+        client_id           INTEGER        NOT NULL PRIMARY KEY UNIQUE
+        , username          VARCHAR(16)    NOT NULL UNIQUE
+        , login_password    VARCHAR(255)   NOT NULL
 );
 
 CREATE TABLE Message
@@ -14,23 +14,31 @@ CREATE TABLE Message
 
 CREATE TABLE Message_Body
 (
-        message_id              INTEGER         NOT NULL PRIMARY KEY UNIQUE
-        , content               TEXT            NOT NULL
+        message_id      INTEGER         NOT NULL PRIMARY KEY UNIQUE
+        , content       TEXT            NOT NULL
 ); 
 
 CREATE TABLE Server_Connection
 (
-
+        username            VARCHAR(16)     UNIQUE
+        , clientPrivateKey  VARCHAR(255)    NOT NULL UNIQUE
+        , aesKey            VARCHAR(255)    UNIQUE
+        , clientPublicKey   VARCHAR(255)    UNIQUE
+        , clientId          VARCHAR(255)    NOT NULL UNIQUE PRIMARY KEY
 );
 
 CREATE TABLE Client
 (
-
+        clientPublicKey     VARCHAR(255)   NOT NULL
+        , clientId          VARCHAR(255)   NOT NULL PRIMARY KEY UNIQUE
 );
 
 CREATE TABLE Server_Keys
 (
-
+        serverId        VARCHAR(255)    NOT NULL UNIQUE PRIMARY KEY
+        , createdAt     TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 SHOW TABLES;
+
+DESC Account;
